3a65c50ed0f4506d6ca185f946fafe5671aed3ab
Wing Ming Sam <wmsam91@gmail.com>
2020-12-02 23:57:30 +0800

    ci

--- ci-build.ps1
+++ ci-build.ps1
@@ -1,4 +1,7 @@
-GOOS=linux go build main.go
+GOOS=linux 
+go build ./chapter/chapter.go
+go build ./list/list.go
+go build ./chapterlist/chapterlist.go 
 mkdir outputs
 zip ./chapter/chapter.zip chapter
 zip ./chapter/chapterlist.zip chapterlist
