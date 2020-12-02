ed8d8d2fdfb277328b4b8f90b2a34b4d3467500e
Sam Wing Ming <45964854+wingmingsam@users.noreply.github.com>
2020-12-03 04:19:52 +0800

    ci

--- ci-build.ps1
+++ ci-build.ps1
@@ -2,7 +2,7 @@ GOOS=linux
 go build -o output/novels ./novels/novels.go
 go build -o output/chapters ./chapters/chapters.go
 go build -o output/chapter ./chapter/chapter.go 
-mkdir zip
-zip ./zip/novels.zip ./output/novels
-zip ./zip/chapters.zip ./output/chapters
-zip ./zip/chapter.zip ./output/chapter
+cd output
+zip ./novels.zip ./novels
+zip ./chapters.zip ./chapters
+zip ./chapter.zip ./chapter
