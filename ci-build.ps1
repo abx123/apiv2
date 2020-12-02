b1d3421349bc4711c752ac8ede8d603807400d3e
Wing Ming Sam <wmsam91@gmail.com>
2020-12-03 03:41:28 +0800

    ci

--- ci-build.ps1
+++ ci-build.ps1
@@ -1,7 +1,7 @@
 GOOS=linux 
 go build -o output/novels ./chapter/novels.go
 go build -o output/chapters ./list/chapters.go
-go build -o output/chapter ./chapterlist/chapter.go 
+go build -o output/chapter ./chapter/chapter.go 
 mkdir zip
 zip ./zip/novels.zip ./output/novels
 zip ./zip/chapters.zip ./output/chapters
